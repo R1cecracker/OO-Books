ddd947b6d2ec386c7e93026b7bbaf3ed90343f4d
R1cecracker <troy.harcoan0@gmail.com>
2025-05-15 00:05:16 +0000

    created classes

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-Write Python code for both the Novel and Magazine classes modelled in the previous slide. Include a suitable constructor method which uses the Book constructor method. Instantiate 2 novels and 2 magazines and print their details.
-Create the Book class (plus methods and attributes)
-Create the Novel class that inherits from Book class.
-Create the Magazine class that inherits from Book class.
+# Write Python code for both the Novel and Magazine classes modelled in the previous slide. Include a suitable constructor method which uses the Book constructor method. Instantiate 2 novels and 2 magazines and print their details.
+# Create the Book class (plus methods and attributes)
+# Create the Novel class that inherits from Book class.
+# Create the Magazine class that inherits from Book class.
+class Book():
+    def __init__(self):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.numPages = numPages
+    
+    def self.displayDetails():
+    
+    def self.rateBook(rating):
+
+    def self.reviewBook(review)
+
+class Novel(Book):
+    def __init__(self):
+        self.genre = genre
+        self.numChapters = numChapters
+    
+    def self.calcReadTime(readSpeed):
+
+class Magazine(Book):
+    def __init__(self):
+        self.issueNumber = issueNumber
+        self.numArticles = numArticles
+    
+    def self.getArticleByTitle():
+        
